a62783fa122ac9f6458af627b7f5ee376269a0df
DTripleD <tripledpadik@gmail.com>
2023-03-10 16:02:45 +0200

    start making 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".feedback-form");
 
+const STORAGE_KEY = "feedback-form-state";
+
+
+form.addEventListener("input", (event) => {
+    event.preventDefault();
+
+    const {email, message} = event.currentTarget;
+
+    const data = {
+        email: email.value,
+        message: message.value,
+    }
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+
+    updateOutput()
+
+
+});
+
+
+function updateOutput(event) {
+    const {email, message} = event.currentTarget;
+    email.textContent = localStorage.getItem(STORAGE_KEY) || "";
+}
