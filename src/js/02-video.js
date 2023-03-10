a3bf891123e012600a544d03f3e1be542a221272
DTripleD <tripledpadik@gmail.com>
2023-03-10 15:40:24 +0200

    make second task (videoplayer)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_TIME_KEY = 'videoplayer-current-time';
+const timeValueFromStorage = localStorage.getItem(STORAGE_TIME_KEY)
+  ? localStorage.getItem(STORAGE_TIME_KEY)
+  : 0;
+
+player.on('timeupdate', throttle(timeSaver, 1000));
+
+player.setCurrentTime(timeValueFromStorage);
+
+
+
+
+function timeSaver({ seconds }){
+    localStorage.setItem(STORAGE_TIME_KEY, seconds);
+}
 
