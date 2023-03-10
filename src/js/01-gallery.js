b06528a273d547b937a00f2c6b2d9d82b98c4c16
DTripleD <tripledpadik@gmail.com>
2023-03-10 14:28:36 +0200

    make 1 task (gallery on npm lib)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+            data-src="${original}">
+        </a>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+  scrollZoom: false,
+});
